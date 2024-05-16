8caa7301c78d6943f816e2f5d8d6e6fd9b86db30
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2024-05-16 15:55:58 +0900

    arom

--- spring-data-jpa-1/initial/src/main/java/cholog/CustomerRepository.java
+++ spring-data-jpa-1/initial/src/main/java/cholog/CustomerRepository.java
@@ -1,5 +1,17 @@
 package cholog;
 
-public interface CustomerRepository {
+import jakarta.persistence.EntityManager;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    List<Customer> findByLastNameIgnoreCase(String lastName);
+
+    List<Customer> findByLastNameOrderByFirstNameDesc(String lastName);
+
 
 }
